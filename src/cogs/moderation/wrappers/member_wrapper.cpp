7981cf8b277ef8644964bcf71bef7008109f801c
AA1999 <arshia.aghaei@gmail.com>
2023-12-06 18:43:36 +0330

    Added docstrings for the class member_wrapper, removed unnecessary exception usage, made the default destructor abstract.

--- src/cogs/moderation/wrappers/member_wrapper.cpp
+++ src/cogs/moderation/wrappers/member_wrapper.cpp
@@ -3,15 +3,6 @@
 //
 
 #include "member_wrapper.h"
-#include "../../../exceptions/not_implemented.h"
-
-void member_wrapper::check_permissions() {
-	throw not_implemented{};
-}
-
-void member_wrapper::wrapper_function() {
-	throw not_implemented{};
-}
 
 bool member_wrapper::are_all_errors() const {
 	return members_with_errors.size() == members.size();
--- src/cogs/moderation/wrappers/member_wrapper.h
+++ src/cogs/moderation/wrappers/member_wrapper.h
@@ -9,15 +9,34 @@
 #include <dpp/dpp.h>
 #include <vector>
 
+/**
+ * @brief member_wrapper - A wrapper used to process commands with only dpp::guild_member elements.
+ */
 class member_wrapper: public base_wrapper {
 protected:
 	std::vector<dpp::guild_member> members;
 	std::vector<dpp::guild_member> members_with_errors;
 
-	void check_permissions() override;
-	void wrapper_function() override;
+	/**
+	 * 	@brief check_permissions - Checks if the user issuing the wrapper has the sufficient permission.
+	 * 	@note This is an abstract function.
+	 */
+	void check_permissions() override = 0;
+
+	/**
+	 * 	@brief wrapper_function - The main function that manages every internal working of the wrapper and the three processes that are performed.
+	 * 	@note This is an abstract function.
+	 */
+	void wrapper_function() override = 0;
 
 public:
+	~member_wrapper() override = 0;
+
 	member_wrapper(const std::vector<dpp::guild_member>& members, moderation_command& command): members(members), base_wrapper(std::move(command)){}
+
+	/**
+	 * @brief are_all_errors - Checks if every item has encountered an error.
+	 * @return Whether if every single item given to the wrapper has encountered an error.
+	 */
 	[[nodiscard]] bool are_all_errors() const override;
 };
