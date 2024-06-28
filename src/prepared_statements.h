7eac637c80d3e3f82d7289e24c8acc4ec026d25a
AA1999 <arshia.aghaei@gmail.com>
2024-06-28 10:08:47 +0330

    Included a new prepared statement to clear the the ban remove days option.

--- src/prepared_statements.h
+++ src/prepared_statements.h
@@ -13,7 +13,6 @@ constexpr std::uint8_t MAP_SIZE{30};
 namespace reactaio::internal {
 	extern inline const fixed_map<std::string, std::string, MAP_SIZE> prepated_statements {
 		{"kick_modlog", "SELECT member_kick, modlog, public_modlog FROM config WHERE guild_id = $1"},
-
 		{"casecount", "SELECT case_id FROM modcase WHERE guild_id = $1 ORDER BY case_id DESC LIMIT 1"},
 		{"ban_modlog", "SELECT member_ban_add, modlog, public_modlog FROM config WHERE guild_id = $1"},
 		{"channel_modlog", "SELECT channel_overwrite_create, channel_overwrite_update, channel_overwrite_delete, modlog, public_modlog FROM config WHERE guild_id = $1"},
@@ -28,6 +27,7 @@ namespace reactaio::internal {
 		{"command_insert", "INSERT INTO command_logs(guild_id, author_id, name, issues_at) VALUES($1, $2, $3, $4)"},
 		{"get_ban_remove_days", "SELECT ban_remove_days FROM config WHERE guild_id = $1"},
 		{"update_ban_remove_days", "UPDATE config SET ban_remove_days = $1 WHERE guild_id = $2"},
+		{"delete_ban_remove_days", "DELETE ban_remove_days FROM config WHERE guild_id = $1"},
 		{"get_ban_id", "SELECT ban_id FROM tempbans WHERE guild_id = $1 ORDER BY ban_id DESC LIMIT 1"},
 		{"get_ban", "SELECT ban_id, user_id, guild_id, mod_id, start_date, end_date, reason FROM tempbans WHERE user_id = $1 AND guild_id = $2"},
 		{"tempban", "INSERT INTO tempbans(ban_id, user_id, guild_id, mod_id, start_date, end_date, reason) VALUES($1, $2, $3, $4, $5, $6, $7) ON CONFLICT ON CONSTRAINT tempbans_user_id_guild_id_key DO UPDATE SET start_date = $5, end_date = $6, reason = $7"},
