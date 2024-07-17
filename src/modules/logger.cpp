e34f4ad61c6438834e95ca1d6c05d939d22ce12d
AA1999 <arshia.aghaei@gmail.com>
2024-07-17 15:48:41 +0330

    Added logging as a module to the bot instead of the header/source.

---
+++ src/modules/logger.cpp
@@ -0,0 +1,51 @@
+//
+// Created by arshia on 7/17/24.
+//
+
+#include "logger.h"
+
+namespace reactaio {
+	void logger::innit(const module_map &modules) {
+		ushort constexpr EIGHT_MEGABYTES = 8192;
+		uint constexpr FIVE_GIGABYTES = 1024 * 1024 * 5;
+		spdlog::init_thread_pool(EIGHT_MEGABYTES, 2);
+		std::vector<spdlog::sink_ptr> sinks;
+		std::string const log_file_name = std::format("reactaio-{%Y-%m-%d %H:%M:%S}.log", std::chrono::system_clock::now());
+		auto stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+		auto rotating = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(log_file_name, FIVE_GIGABYTES, 10);
+		sinks.push_back(stdout_sink);
+		sinks.push_back(rotating);
+		auto logger = std::make_shared<spdlog::async_logger>("logs", sinks.begin(), sinks.end(), spdlog::thread_pool(), spdlog::async_overflow_policy::block);
+		spdlog::register_logger(logger);
+		logger->set_pattern("%^%Y-%m-%d %H:%M:%S.%e [%L] [th#%t]%$ : %v");
+		logger->set_level(spdlog::level::level_enum::debug);
+	}
+
+	void logger::start() {} // No specific impl
+
+	void logger::stop() {} // No specific impl
+
+	void logger::info(const std::string_view message) const {
+		m_logger->info(message);
+	}
+
+	void logger::trace(const std::string_view message) const {
+		m_logger->trace(message);
+	}
+
+	void logger::debug(const std::string_view message) const {
+		m_logger->debug(message);
+	}
+
+	void logger::warn(const std::string_view message) const {
+		m_logger->warn(message);
+	}
+
+	void logger::error(const std::string_view message) const {
+		m_logger->error(message);
+	}
+
+	void logger::critical(const std::string_view message) const {
+		m_logger->critical(message);
+	}
+}// namespace reactaio
---
+++ src/modules/logger.h
@@ -0,0 +1,72 @@
+//
+// Created by arshia on 7/17/24.
+//
+
+#pragma once
+#include "../cogs/core/datatypes/module.h"
+
+#include <spdlog/spdlog.h>
+#include <spdlog/async.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+
+namespace reactaio {
+	class logger final: public module {
+		std::shared_ptr<spdlog::async_logger> m_logger;
+	public:
+		logger(): module("logger", dependency_t{}){};
+		~logger() override = default;
+
+		/**
+		 * @brief Initializes the moderation module with the provided modules.
+		 * @param modules A reference of a map of modules.
+		 */
+		void innit(const module_map& modules) override;
+
+		/**
+		 * @brief Starts the module.
+		 */
+		void start() override;
+
+		/**
+		 * @brief Stops the module.
+		 */
+		void stop() override;
+
+		/**
+		 * @brief Provides an information log.
+		 * @param message The message to log.
+		 */
+		void info(std::string_view message) const;
+
+		/**
+		 * @brief Provides a trace log.
+		 * @param message The message to log.
+		 */
+		void trace(std::string_view message) const;
+
+		/**
+		 * @brief Provides a debug log.
+		 * @param message The message to log.
+		 */
+		void debug(std::string_view message) const;
+
+		/**
+		 * @brief Provides a warning log.
+		 * @param message The message to log.
+		 */
+		void warn(std::string_view message) const;
+
+		/**
+		 * @brief Provides an error log.
+		 * @param message The message to log.
+		 */
+		void error(std::string_view message) const;
+
+		/**
+		 * @brief Provides a critical log.
+		 * @param message The message to log.
+		 */
+		void critical(std::string_view message) const;
+	};
+}
