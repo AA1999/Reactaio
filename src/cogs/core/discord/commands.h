f57f17d2fa050cfef10ef109bd035a5bc1ad6b0f
AA1999 <arshia.aghaei@gmail.com>
2024-08-26 23:50:34 +0330

    Added docs to the functions.

--- src/cogs/core/discord/commands.h
+++ src/cogs/core/discord/commands.h
@@ -7,12 +7,21 @@
 #include "slash_command_properties.h"
 
 namespace reactaio::moderation {
+
 	/**
 	 * @brief Creates a global discord command for the bot.
-	 * @param properties Properties of the slash command: name, description and a pointer to the bot.
+	 * @param properties A struct wrapping the constructor for dpp::slash_command.
 	 * @param options A list of command options.
 	 * @return The resulting slash command.
 	 */
 	inline dpp::slashcommand define_command(const slash_command_properties &properties, const std::vector<dpp::command_option> &options);
+
+
+	/**
+	 * @brief Creates a guild discord command.
+	 * @param properties A struct wrapping the constructor for dpp::slash_command.
+	 * @param options A list of command options.
+	 * @param guild_id The guild to register the command to.
+	 */
 	inline void define_guild_command(const slash_command_properties &properties, const std::vector<dpp::command_option> &options, dpp::snowflake const &guild_id);
 }
