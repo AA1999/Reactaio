792f8578d221748c09676306a33e68f290c53147
AA1999 <arshia.aghaei@gmail.com>
2024-09-15 11:19:12 +0330

    Fixed and improved documentation for hardban_wrapper.h and kick_wrapper.h

--- src/cogs/moderation/wrappers/hardban/hardban_wrapper.h
+++ src/cogs/moderation/wrappers/hardban/hardban_wrapper.h
@@ -8,33 +8,33 @@
 
 
 /**
- * @brief hardban_wrapper - Wrapper class to hard ban a series of users and members.
+ * @brief Wrapper class to hard ban a series of users and members.
  * @note Due to security reasons, this can only be called by the server owner.
  */
 class hardban_wrapper final: public hybrid_wrapper {
 
 	/**
-	 * @brief wrapper_function - The function that is called on operator() call and will process all the required steps to perform the operation.
+	 * @brief The function that is called on operator() call and will process all the required steps to perform the operation.
 	 */
 	void wrapper_function() override;
 
 	/**
-	 * @brief check_permissions - Checks if the harban operation(s) can be performed (bot perms, author being owner etc)
+	 * @brief Checks if the harban operation(s) can be performed (bot perms, author being owner etc)
 	 */
 	void check_permissions() override;
 
 	/**
-	 * @brief process_softbans - Starts the hardban operation on all the given users/members.
+	 * @brief Starts the hardban operation on all the given users/members.
 	 */
 	void process_hardbans();
 
 	/**
-	 * @brief process_response - Puts the wrapper result (errors + success) to the reply message.
+	 * @brief Puts the wrapper result (errors + success) to the reply message.
 	 */
 	void process_response();
 
 	/**
-	 * @brief lambda_callback - This is a function that's called when an API call is made.
+	 * @brief This is a function that's called when an API call is made.
 	 * @param completion On success the callback will contain a dpp::confirmation object in confirmation_callback_t::value. On failure, the value is undefined and confirmation_callback_t::is_error() method will return true.
 	 * @param user User object that the callback is made on.
 	 */
--- src/cogs/moderation/wrappers/kick/kick_wrapper.h
+++ src/cogs/moderation/wrappers/kick/kick_wrapper.h
@@ -6,33 +6,33 @@
 #include "../member_wrapper.h"
 
 /**
- * @brief kick_wrapper - Functor that processes the kick operation on a list of dpp::guild_member objects.
+ * @brief Functor that processes the kick operation on a list of dpp::guild_member objects.
  */
 class kick_wrapper final: public member_wrapper {
 	/**
-	 * @brief wrapper_function - Function called when the () operator is called. Kicks all the given members from the guild and returns any errors/success message.
+	 * @brief Function called when the () operator is called. Kicks all the given members from the guild and returns any errors/success message.
 	 */
 	void wrapper_function() override;
 
 	/**
-	 * @brief check_permissions - Checks if the command invoker has the appropriate permissions to perform the operation (including the bot itself).
+	 * @brief Checks if the command invoker has the appropriate permissions to perform the operation (including the bot itself).
 	 * @note This is called internally and will return the result via the wrapper_function() function.
 	 */
 	void check_permissions() override;
 
 	/**
-	 * @brief process_kicks - Kicks the list of dpp::guild_member from the guild. Shows all errors or success message.
+	 * @brief Kicks the list of dpp::guild_member from the guild. Shows all errors or success message.
 	 * @note This is called internally and will return the result via the wrapper_function() function.
 	 */
 	void process_kicks();
 
 	/**
-	 * @brief process_response - Sends the resulting response to the wrapper message object as embed(s).
+	 * @brief Sends the resulting response to the wrapper message object as embed(s).
 	 */
 	void process_response();
 
 	/**
-	 * @brief lambda_callback - This is a function that's called when an API call is made.
+	 * @brief This is a function that's called when an API call is made.
 	 * @param completion On success the callback will contain a dpp::confirmation object in confirmation_callback_t::value. On failure, the value is undefined and confirmation_callback_t::is_error() method will return true.
 	 * @param member Member object that the callback is made on.
 	 */
