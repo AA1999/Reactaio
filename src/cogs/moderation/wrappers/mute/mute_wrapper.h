e8da3e64a144bc4a42c72c24a8c22711e87e8235
AA1999 <arshia.aghaei@gmail.com>
2024-09-16 22:46:00 +0330

    Fixed and improved documentation for mute_wrapper.h and softban_wrapper.h

--- src/cogs/moderation/wrappers/mute/mute_wrapper.h
+++ src/cogs/moderation/wrappers/mute/mute_wrapper.h
@@ -8,35 +8,35 @@
 
 
 /**
- * @brief mute_wrapper - Functor that processes the mute/timeout operation on a list of dpp::guild_member objects.
+ * @brief Functor that processes the mute/timeout operation on a list of dpp::guild_member objects.
  */
 class mute_wrapper final: public member_wrapper {
 	/**
-	 * @brief wrapper_function - Function called when the () operator is called. Mutes/Times out all the given members in the guild and returns any errors/success message.
+	 * @brief Function called when the () operator is called. Mutes/Times out all the given members in the guild and returns any errors/success message.
 	 */
 	void wrapper_function() override;
 
 	/**
-	 * @brief check_permissions - Checks if the command invoker has the appropriate permissions to perform the operation (including the bot itself).
+	 * @brief Checks if the command invoker has the appropriate permissions to perform the operation (including the bot itself).
 	 * @note This is called internally and will return the result via the wrapper_function() function.
 	 */
 	void check_permissions() override;
 
 	/**
-	 * @brief process_mutes - Mutes the list of dpp::guild_member. Shows all errors or success message.
+	 * @brief Mutes the list of dpp::guild_member. Shows all errors or success message.
 	 * @note This is called internally and will return the result via the wrapper_function() function.
 	 */
 	void process_mutes();
 
 	/**
-	 * @brief process_response - Sends the resulting response to the wrapper message object as embed(s).
+	 * @brief Sends the resulting response to the wrapper message object as embed(s).
 	 */
 	void process_response();
 
 	bool use_timeout{true};
 
 	/**
-	 * @brief lambda_callback - This is a function that's called when an API call is made.
+	 * @brief This is a function that's called when an API call is made.
 	 * @param completion On success the callback will contain a dpp::confirmation object in confirmation_callback_t::value. On failure, the value is undefined and confirmation_callback_t::is_error() method will return true.
 	 * @param member Member object that the callback is made on.
 	 */
--- src/cogs/moderation/wrappers/softban/softban_wrapper.h
+++ src/cogs/moderation/wrappers/softban/softban_wrapper.h
@@ -6,29 +6,32 @@
 
 #include "../hybrid_wrapper.h"
 
+/**
+ * @brief A functor wrapper for handling softbans.
+ */
 class softban_wrapper: public hybrid_wrapper {
 	/**
-	 * @brief wrapper_function - The function that is called on operator() call and will process all the required steps to perform the operation.
+	 * @brief The function that is called on operator() call and will process all the required steps to perform the operation.
 	 */
 	void wrapper_function() override;
 
 	/**
-	 * @brief check_permissions - Checks if the softban operation(s) can be performed (bot perms, author being owner etc)
+	 * @brief Checks if the softban operation(s) can be performed (bot perms, author being owner etc)
 	 */
 	void check_permissions() override;
 
 	/**
-	 * @brief process_softbans - Starts the softban operation on all the given users/members.
+	 * @brief Starts the softban operation on all the given users/members.
 	 */
 	void process_softbans();
 
 	/**
-	 * @brief process_response - Puts the wrapper result (errors + success) to the reply message.
+	 * @brief Puts the wrapper result (errors + success) to the reply message.
 	 */
 	void process_response();
 
 	/**
-	 * @brief lambda_callback - This is a function that's called when an API call is made.
+	 * @brief This is a function that's called when an API call is made.
 	 * @param completion On success the callback will contain a dpp::confirmation object in confirmation_callback_t::value. On failure, the value is undefined and confirmation_callback_t::is_error() method will return true.
 	 * @param user User object that the callback is made on.
 	 */
