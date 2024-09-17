2996f3d135c553b68df17074cb7227bd6ff0fddb
AA1999 <arshia.aghaei@gmail.com>
2024-09-17 11:02:34 +0330

    Fixed and improved documentation for unban_wrapper.h and unmute_wrapper.h and removed unused includes.

--- src/cogs/moderation/wrappers/unban/unban_wrapper.h
+++ src/cogs/moderation/wrappers/unban/unban_wrapper.h
@@ -5,36 +5,35 @@
 #pragma once
 
 #include "../user_wrapper.h"
-#include <vector>
 
 /**
- * @brief unban_wrapper - Functor that processes the unban operation for a list of dpp::user* objects.
+ * @brief Functor that processes the unban operation for a list of dpp::user* objects.
  */
 class unban_wrapper final: public user_wrapper {
 	/**
-	 * @brief wrapper_function - Function called when the () operator is called. Unbans all the given members/users from the guild and returns any errors/success message.
+	 * @brief Function called when the () operator is called. Unbans all the given members/users from the guild and returns any errors/success message.
 	 */
 	void wrapper_function() override;
 
 	/**
-	 * @brief check_permissions - Checks if the command invoker has the permission to unban anyone from the guild (including the bot).
+	 * @brief Checks if the command invoker has the permission to unban anyone from the guild (including the bot).
 	 * @note This function works internally and returns all error to the wrapper_function() function.
 	 */
 	void check_permissions() override;
 
 	/**
-	 * @brief process_unbans - Unbans the list of users/members from the guild and returns errors/success message.
+	 * @brief Unbans the list of users/members from the guild and returns errors/success message.
 	 * @note This function works internally and returns all error to the wrapper_function() function.
 	 */
 	void process_unbans();
 
 	/**
-	 * @brief process_response - Sends the resulting response to the wrapper message object as embed(s).
+	 * @brief Sends the resulting response to the wrapper message object as embed(s).
 	 */
 	void process_response();
 
 	/**
-	 * @brief lambda_callback - This is a function that's called when an API call is made.
+	 * @brief This is a function that's called when an API call is made.
 	 * @param completion On success the callback will contain a dpp::confirmation object in confirmation_callback_t::value. On failure, the value is undefined and confirmation_callback_t::is_error() method will return true.
 	 * @param user User object that the callback is made on.
 	 */
