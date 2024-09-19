a40d5f748d70e87199af67a484d3786af797024e
AA1999 <arshia.aghaei@gmail.com>
2024-09-19 15:37:53 +0330

    Fixed and improved documentation for warn_wrapper.h

--- src/cogs/moderation/wrappers/warn/warn_wrapper.h
+++ src/cogs/moderation/wrappers/warn/warn_wrapper.h
@@ -7,33 +7,33 @@
 #include "../member_wrapper.h"
 
 /**
- * @brief warn_wrapper - Functor that processes the warn operation for a list of dpp::guild_member objects.
+ * @brief  Functor that processes the warn operation for a list of dpp::guild_member objects.
  */
 class warn_wrapper final: public member_wrapper {
 	/**
-	 * @brief wrapper_function - Function called when the () operator is called. Warns all the given members in the guild and returns any errors/success message.
+	 * @brief Function called when the () operator is called. Warns all the given members in the guild and returns any errors/success message.
 	 */
 	void wrapper_function() override;
 
 	/**
-	 * @brief check_permissions - Checks if the command invoker has the permission to warn anyone from the guild (including the bot).
+	 * @brief Checks if the command invoker has the permission to warn anyone from the guild (including the bot).
 	 * @note This function works internally and returns all error to the wrapper_function() function.
 	 */
 	void check_permissions() override;
 
 	/**
-	 * @brief process_bans - Bans the list of users/members from the guild and returns errors/success message.
+	 * @brief Bans the list of users/members from the guild and returns errors/success message.
 	 * @note This function works internally and returns all error to the wrapper_function() function.
 	 */
 	void process_warnings();
 
 	/**
-	 * @brief process_response - Sends the resulting response to the wrapper message object as embed(s).
+	 * @brief Sends the resulting response to the wrapper message object as embed(s).
 	 */
 	void process_response();
 
 	/**
-	 * @brief lambda_callback - This is a function that's called when an API call is made.
+	 * @brief This is a function that's called when an API call is made.
 	 * @param completion On success the callback will contain a dpp::confirmation object in confirmation_callback_t::value. On failure, the value is undefined and confirmation_callback_t::is_error() method will return true.
 	 * @param member Member object that the callback is made on.
 	 */
