72d3e9d8cc5d9594da1503cac92a9a2ee7ede5b5
AA1999 <arshia.aghaei@gmail.com>
2024-09-28 15:21:08 +0330

    Fixed and improved documentation for view_guild_warnings.h

--- src/cogs/moderation/wrappers/warning management/view_guild_warnings.h
+++ src/cogs/moderation/wrappers/warning management/view_guild_warnings.h
@@ -8,23 +8,23 @@
 
 
 /**
- * @brief view_guild_warnings - Wrapper to handle viewing all guild warnings.
+ * @brief Wrapper to handle viewing all guild warnings.
  */
 class view_guild_warnings final: public simple_wrapper {
 	dpp::message response;
 
 	/**
-	 * 	@brief check_permissions - Checks if the user issuing the wrapper has the sufficient permission.
+	 * 	@brief Checks if the user issuing the wrapper has the sufficient permission.
 	 */
 	void check_permissions() override;
 
 	/**
-	 * 	@brief wrapper_function - The main function that manages every internal working of the wrapper and the three processes that are performed.
+	 * 	@brief The main function that manages every internal working of the wrapper and the three processes that are performed.
 	 */
 	void wrapper_function() override;
 
 	/**
-	 * @brief process_response - Sends the resulting response to the wrapper message object as embed(s).
+	 * @brief Sends the resulting response to the wrapper message object as embed(s).
 	 */
 	void process_response();
 
