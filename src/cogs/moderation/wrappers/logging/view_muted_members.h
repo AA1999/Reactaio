73ee3770a63fb9088e94a0bd9f3d927985bec543
AA1999 <arshia.aghaei@gmail.com>
2024-11-09 16:28:33 +0330

    Fixed documentation for view_muted_members

--- src/cogs/moderation/wrappers/logging/view_muted_members.h
+++ src/cogs/moderation/wrappers/logging/view_muted_members.h
@@ -7,23 +7,23 @@
 #include "../simple_wrapper.h"
 
 /**
- * @brief view_muted_members - A wrapper to handle viewing muted/timed out members of a server.
+ * @brief A wrapper to handle viewing muted/timed out members of a server.
  */
 class view_muted_members final: public simple_wrapper {
 	dpp::message response;
 
 	/**
-	 * 	@brief check_permissions - Checks if the user issuing the wrapper has the sufficient permission.
+	 * 	@brief Checks if the user issuing the wrapper has the sufficient permission.
 	 */
 	void check_permissions() override;
 
 	/**
-	 * 	@brief wrapper_function - The main function that manages every internal working of the wrapper and the three processes that are performed.
+	 * 	@brief The main function that manages every internal working of the wrapper and the three processes that are performed.
 	 */
 	void wrapper_function() override;
 
 	/**
-	 * @brief process_response - Sends the resulting response to the wrapper message object as embed(s).
+	 * @brief Sends the resulting response to the wrapper message object as embed(s).
 	 */
 	void process_response();
 public:
