35b24be4ba509c969d25d4ad34f670ea17963fc4
Arshia Aghaei <arshia.aghaei@gmail.com>
2025-01-12 23:56:35 +0330

    Fixed formatting/indent issues, implemented check_ban_possible(). Replaced all appropriate code snippets with invoke_error_webhook(), log_command_invoke, and replaced the invocation of the now-removed helper protected/mod_roles with the new methods defined in command_wrapper.

--- src/cogs/moderation/wrappers/ban/ban_wrapper.cpp
+++ src/cogs/moderation/wrappers/ban/ban_wrapper.cpp
@@ -32,7 +32,7 @@ void ban_wrapper::wrapper_function() {
 		}
 	}
 
-	if (invalid_user) {
+	if(invalid_user) {
 		auto const split = join_with_limit(errors, bot_max_embed_chars);
 		error_message = dpp::message(command.channel_id, "");
 		auto const time_now = std::time(nullptr);
@@ -45,13 +45,13 @@ void ban_wrapper::wrapper_function() {
 			error_message.add_embed(base_embed);
 		}
 		else {
-			for (auto const &error: errors) {
+			for(auto const &error: errors) {
 				auto embed{base_embed};
 				embed.set_description(error);
 				error_message.add_embed(embed);
 			}
 		}
-		if (command.interaction) {
+		if(command.interaction) {
 			error_message.set_flags(dpp::m_ephemeral); // Hidden error message
 			if(split.size() == 1)
 				(*command.interaction)->edit_response(error_message);
@@ -64,7 +64,7 @@ void ban_wrapper::wrapper_function() {
 			auto transaction = pqxx::work{*command.connection};
 			auto error_channel_query = transaction.exec_prepared("botlog", std::to_string(command.guild->id));
 			if(error_channel_query.empty()) {
-				error_message.set_content("This server hasn't set a channel for bot errors. So the errors are being "
+				error_message.set_content("This server hasn't set a channel forbot errors. So the errors are being "
 				                          "sent to your DMs:");
 				command.bot->direct_message_create(command.author->user_id, error_message);
 			}
@@ -86,7 +86,7 @@ void ban_wrapper::wrapper_function() {
 }
 
 void ban_wrapper::lambda_callback(const dpp::confirmation_callback_t &completion, user_ptr const &user) {
-	if (completion.is_error()) {
+	if(completion.is_error()) {
 		auto error = completion.get_error();
 		errors.push_back(std::format("❌ Unable to ban user **{}**. Error code {}: {}.", user->format_username(), error.code, error.human_readable));
 		users_with_errors.insert(user);
@@ -148,8 +148,8 @@ void ban_wrapper::process_bans() {
 
 	auto* author_user = command.author->get_user();
 
-	for (auto const& user : users) {
-		if (command.interaction) { // If this is automod, DMing lots of users WILL result in a ratelimit
+	for(auto const& user : users) {
+		if(command.interaction) { // ifthis is automod, DMing lots of users WILL result in a ratelimit
 
 			std::string dm_message;
 
@@ -182,21 +182,21 @@ void ban_wrapper::process_bans() {
 
 void ban_wrapper::process_response() {
 	auto message = dpp::message(command.channel_id, "");
-	auto const* author_user = command.author->get_user();
+	auto const *author_user = command.author->get_user();
 
-	if (has_error()) {
+	if(has_error()) {
 		auto format_split = join_with_limit(errors, bot_max_embed_chars);
 		auto const time_now = std::time(nullptr);
-		auto base_embed		= dpp::embed()
-				.set_title("Error while banning member(s): ")
-				.set_color(ERROR_COLOR)
-				.set_timestamp(time_now);
+		auto base_embed = dpp::embed()
+								  .set_title("Error while banning member(s): ")
+								  .set_color(ERROR_COLOR)
+								  .set_timestamp(time_now);
 		if(format_split.size() == 1) {
 			base_embed.set_description(format_split[0]);
 			message.add_embed(base_embed);
 		}
 		else {
-			for (auto const& error : format_split) {
+			for(auto const &error: format_split) {
 				auto embed{base_embed};
 				embed.set_description(error);
 				message.add_embed(embed);
@@ -204,7 +204,7 @@ void ban_wrapper::process_response() {
 		}
 		if(command.interaction) {
 			if(are_all_errors())
-				message.set_flags(dpp::m_ephemeral); // Invisible error message.
+				message.set_flags(dpp::m_ephemeral);// Invisible error message.
 			if(format_split.size() == 1)
 				(*command.interaction)->edit_response(message);
 			else {
@@ -212,65 +212,41 @@ void ban_wrapper::process_response() {
 				paginator.start();
 			}
 		}
-		else {
-			// Get bot error webhook
-			auto automod_log = dpp::message();
-			auto transaction  = pqxx::work{*command.connection};
-			auto webhook_url_query = transaction.exec_prepared("botlog", std::to_string(command.guild->id));
-			transaction.commit();
-			if(webhook_url_query.empty()) { // Bot has no error webhook set.
-				automod_log.set_content("This server hasn't set a channel for bot errors. So the errors are being sent to your DMs:");
-				for (auto const& error : format_split) {
-					auto embed{base_embed};
-					embed.set_description(error);
-					automod_log.add_embed(embed);
-				}
-				command.bot->direct_message_create(command.author->user_id, automod_log);
-			}
-			else {
-				auto webhook_url = webhook_url_query[0]["bot_error_logs"].as<std::string>();
-				dpp::webhook automod_webhook{webhook_url};
-				for (auto const& error : format_split) {
-					auto embed{base_embed};
-					embed.set_description(error);
-					automod_log.add_embed(embed);
-				}
-				command.bot->execute_webhook(automod_webhook, automod_log);
-			}
+		else
+			invoke_error_webhook();
 
-		}
 	}
-	if (!are_all_errors()) {
+	if(!are_all_errors()) {
 		shared_vector<dpp::user> banned_users;
 		std::vector<std::string> banned_usernames;
 		std::vector<std::string> banned_mentions;
 
 		reactaio::set_difference(users, users_with_errors, banned_users);
 
-		std::ranges::transform(banned_users, std::back_inserter(banned_usernames), [](user_ptr const& user) {
+		std::ranges::transform(banned_users, std::back_inserter(banned_usernames), [](user_ptr const &user) {
 			return std::format("**{}**", user->format_username());
 		});
 
-		std::ranges::transform(banned_users, std::back_inserter(banned_mentions), [](user_ptr const& user) {
-            return user->get_mention();
+		std::ranges::transform(banned_users, std::back_inserter(banned_mentions), [](user_ptr const &user) {
+			return user->get_mention();
 		});
 
 		auto usernames = join(banned_usernames, ", ");
-		auto mentions  = join(banned_mentions, ", ");
+		auto mentions = join(banned_mentions, ", ");
 
 		std::string title;
 		std::string description;
 		std::string gif_url;
 
-		if (banned_users.size() == 1) {
-			title		= "Banned";
+		if(banned_users.size() == 1) {
+			title = "Banned";
 			description = std::format("{} has been banned", usernames);
-			gif_url		= "https://media3.giphy.com/media/LOoaJ2lbqmduxOaZpS/giphy.gif";
+			gif_url = "https://media3.giphy.com/media/LOoaJ2lbqmduxOaZpS/giphy.gif";
 		}
 		else {
-			title		= "Banned";
+			title = "Banned";
 			description = std::format("{} have been banned", usernames);
-			gif_url		= "https://i.gifer.com/1Daz.gif";
+			gif_url = "https://i.gifer.com/1Daz.gif";
 		}
 
 		if(duration) {
@@ -278,20 +254,20 @@ void ban_wrapper::process_response() {
 			auto time_delta = duration->to_seconds();
 			auto future = time_now + time_delta;
 			std::string time_future_relative = timestamp(future.time_since_epoch().count(),
-			                                                           dpp::utility::time_format::tf_relative_time);
+														 dpp::utility::time_format::tf_relative_time);
 			description.append(std::format(" until {}.", time_future_relative));
 		}
 		else
 			description.append(".");
 
-		auto time_now= std::time(nullptr);
+		auto time_now = std::time(nullptr);
 
 		auto response = dpp::embed()
-				.set_color(RESPONSE_COLOR)
-				.set_title(title)
-				.set_description(description)
-				.set_image(gif_url)
-				.set_timestamp(time_now);
+								.set_color(RESPONSE_COLOR)
+								.set_title(title)
+								.set_description(description)
+								.set_image(gif_url)
+								.set_timestamp(time_now);
 		response.add_field("Moderator: ", author_user->get_mention());
 		response.add_field("Reason: ", command.reason);
 
@@ -321,20 +297,20 @@ void ban_wrapper::process_response() {
 				auto time_delta = duration->to_seconds();
 				auto future = now + time_delta;
 				std::string time_future_relative = timestamp(future.time_since_epoch().count(),
-				                                                           dpp::utility::time_format::tf_relative_time);
+															 dpp::utility::time_format::tf_relative_time);
 				description.append(std::format(" until {}.", time_future_relative));
 			}
 			else
 				description.append(".");
 			time_now = std::time(nullptr);
 			auto ban_log = dpp::embed()
-					.set_color(LOG_COLOR)
-					.set_title(embed_title)
-					.set_thumbnail(embed_image_url)
-					.set_timestamp(time_now)
-					.set_description(description)
-					.add_field("Moderator: ", command.author->get_mention())
-					.add_field("Reason: ", std::string{command.reason});
+								   .set_color(LOG_COLOR)
+								   .set_title(embed_title)
+								   .set_thumbnail(embed_image_url)
+								   .set_timestamp(time_now)
+								   .set_description(description)
+								   .add_field("Moderator: ", command.author->get_mention())
+								   .add_field("Reason: ", std::string{command.reason});
 			dpp::message log{command.channel_id, ""};
 			log.add_embed(ban_log);
 
@@ -357,21 +333,21 @@ void ban_wrapper::process_response() {
 				auto time_delta = duration->to_seconds();
 				auto future = now + time_delta;
 				std::string time_future_relative = timestamp(future.time_since_epoch().count(),
-				                                                           dpp::utility::time_format::tf_relative_time);
+															 dpp::utility::time_format::tf_relative_time);
 				description.append(std::format(" until {}.", time_future_relative));
-			}
+			} 
 			else
 				description.append(".");
 
 			time_now = std::time(nullptr);
 			auto ban_log = dpp::embed()
-					.set_color(LOG_COLOR)
-					.set_title(embed_title)
-					.set_thumbnail(embed_image_url)
-					.set_timestamp(time_now)
-					.set_description(description)
-					.add_field("Moderator: ", command.author->get_mention())
-					.add_field("Reason: ", std::string{command.reason});
+								   .set_color(LOG_COLOR)
+								   .set_title(embed_title)
+								   .set_thumbnail(embed_image_url)
+								   .set_timestamp(time_now)
+								   .set_description(description)
+								   .add_field("Moderator: ", command.author->get_mention())
+								   .add_field("Reason: ", std::string{command.reason});
 			dpp::message log{command.channel_id, ""};
 			log.add_embed(ban_log);
 			command.bot->execute_webhook(modlog_webhook, log);
@@ -392,50 +368,39 @@ void ban_wrapper::process_response() {
 				auto time_delta = duration->to_seconds();
 				auto future = now + time_delta;
 				std::string time_future_relative = timestamp(future.time_since_epoch().count(),
-				                                                           dpp::utility::time_format::tf_relative_time);
+															 dpp::utility::time_format::tf_relative_time);
 				description.append(std::format(" until {}.", time_future_relative));
 			}
 			else
 				description.append(".");
 			time_now = std::time(nullptr);
 			auto ban_log = dpp::embed()
-					.set_color(LOG_COLOR)
-					.set_title(embed_title)
-					.set_thumbnail(embed_image_url)
-					.set_timestamp(time_now)
-					.set_description(description)
-					.add_field("Moderator: ", command.author->get_mention())
-					.add_field("Reason: ", std::string{command.reason});
+								   .set_color(LOG_COLOR)
+								   .set_title(embed_title)
+								   .set_thumbnail(embed_image_url)
+								   .set_timestamp(time_now)
+								   .set_description(description)
+								   .add_field("Moderator: ", command.author->get_mention())
+								   .add_field("Reason: ", std::string{command.reason});
 			dpp::message log{command.channel_id, ""};
 			log.add_embed(ban_log);
 			command.bot->execute_webhook(public_modlog_webhook, log);
 		}
 	}
-	if (command.interaction) {
+	if(command.interaction) {
 		if(message.embeds.size() == 1)
-			(*	command.interaction)->edit_response(message);
+			(*command.interaction)->edit_response(message);
 		else {
 			message_paginator paginator{message, command};
 			paginator.start();
 		}
-		// Log command call
-		pqxx::work transaction{*command.connection};
-		transaction.exec_prepared("command_insert", std::to_string(command.guild->id), std::to_string(command.author->user_id),
-		                          internal::mod_action_name::BAN, dpp::utility::current_date_time());
-		transaction.commit();
+		log_command_invoke(internal::mod_action_name::BAN);
 	}
 	else
-		command.bot->message_create(message);
+        command.bot->message_create(message);
 }
 
-void ban_wrapper::check_permissions() {
-
-	if(members.empty() && users.empty()) {// No point in all this if there's no member/user in that vector
-		cancel_operation = true;
-		errors.emplace_back("❌ Error while parsing the list. It's possible that the provided ids were all invalid.");
-		return;
-	}
-
+void ban_wrapper::check_ban_possible(shared_vector<dpp::role> const& protected_roles) {
 	auto const bot_member = find_guild_member(command.guild->id, command.bot->me.id);
 	auto const bot_roles = get_roles_sorted(bot_member);
 	auto const& bot_top_role = bot_roles.front();
@@ -444,37 +409,16 @@ void ban_wrapper::check_permissions() {
 
 	bool is_owner{false};
 
-	pqxx::work transaction{*command.connection};
-	auto protected_roles_query = transaction.exec_prepared("protected_roles", std::to_string(command.guild->id));
-	transaction.commit();
+	for(auto const &member: members) {
+		auto const member_roles = get_roles_sorted(*member);
+		auto const& member_top_role = member_roles.front();
 
-	shared_vector<dpp::role> protected_roles;
-
-	if(!protected_roles_query.empty()) {
-		auto protected_roles_field = protected_roles_query[0]["protected_roles"];
-		internal::unique_vector<dpp::snowflake> protected_role_snowflakes = parse_psql_array<dpp::snowflake>(protected_roles_field);
-		reactaio::transform(protected_role_snowflakes, protected_roles, [](dpp::snowflake const role_id) {
-			return std::make_shared<dpp::role>(*find_role(role_id));
-		});
-	}
-
-	if(!bot_top_role->has_ban_members()) {
-		cancel_operation = true;
-		errors.emplace_back("❌ Bot doesn't have the appropriate permissions. Please make sure the Ban Members permission is enabled.");
-	}
-
-	//TODO Check permission if the user has Ban members or is a moderator by role (adding this in the db)
-
-	for(auto const& member: members) {
-		auto member_roles = get_roles_sorted(*member);
-		auto member_top_role = *member_roles.begin();
-
-		if(command.author->user_id == member->user_id) { // If for some reason you decided to ban yourself lol
+		if(command.author->user_id == member->user_id) {// ifforsome reason you decided to ban yourself lol
 			if(member->is_guild_owner()) {
 				errors.emplace_back("❌ Why are you banning yourself, server owner? lmfao");
 				is_owner = true;
 			}
-			else {
+		else {
 				errors.emplace_back("❌ You can't ban yourself lmao.");
 			}
 			cancel_operation = true;
@@ -486,7 +430,7 @@ void ban_wrapper::check_permissions() {
 		}
 
 
-		if(command.bot->me.id == member->user_id) { // If you decided to ban the bot (ReactAIO)
+		if(command.bot->me.id == member->user_id) {// ifyou decided to ban the bot (ReactAIO)
 			errors.emplace_back("❌ Can't ban myself lmfao.");
 			cancel_operation = true;
 		}
@@ -495,11 +439,10 @@ void ban_wrapper::check_permissions() {
 
 			shared_vector<dpp::role> member_protected_roles;
 			reactaio::set_intersection(protected_roles, member_roles, member_protected_roles);
-			if(!member_protected_roles.empty()) { // IfR member has any of the protected roles.
+			if(!member_protected_roles.empty()) {// IfR member has any of the protected roles.
 				cancel_operation = true;
 				std::vector<std::string> role_mentions;
-				std::ranges::transform(member_protected_roles, std::back_inserter
-						(role_mentions), [](const role_ptr& role){
+				std::ranges::transform(member_protected_roles, std::back_inserter(role_mentions), [](const role_ptr &role) {
 					return role->get_mention();
 				});
 				std::string role_mentions_str = join(role_mentions, " , ");
@@ -508,32 +451,66 @@ void ban_wrapper::check_permissions() {
 		}
 
 		if(member_top_role->position > bot_top_role->position) {
-			errors.push_back(std::format("❌ {} has a higher role than the bot. Unable to ban. Please "
-			                                "move the bot role above the members and below your staff roles.",
-			                                member->get_mention()));
+			errors.push_back(std::format("❌ {} has a higher role than the bot. Unable to ban. "
+													"Please move the bot role above the members and below your staff roles.",
+											member->get_mention()));
 			cancel_operation = true;
 		}
 
 		if(member_top_role->position > author_top_role->position) {
 			errors.push_back(std::format("❌ {} has a higher role than you do. You can't ban them.",
-			                                member->get_mention()));
+										 member->get_mention()));
 			cancel_operation = true;
 		}
 	}
+}
+
+void ban_wrapper::check_permissions() {
+
+	if(members.empty() && users.empty()) {// No point in all this ifthere's no member/user in that vector
+		cancel_operation = true;
+		errors.emplace_back("❌ Error while parsing the list. It's possible that the provided ids were all invalid.");
+		return;
+	}
+
+	auto const bot_member = find_guild_member(command.guild->id, command.bot->me.id);
+	auto const bot_roles = get_roles_sorted(bot_member);
+	auto const& bot_top_role = bot_roles.front();
+	auto const author_roles = get_roles_sorted(*command.author);
+	auto const& author_top_role = author_roles.front();
+
+	pqxx::work transaction{*command.connection};
+	auto const protected_roles = get_protected_roles();
+
+	if(!bot_top_role->has_ban_members()) {
+		cancel_operation = true;
+		errors.emplace_back("❌ Bot doesn't have the appropriate permissions. Please make sure the Ban Members permission is enabled.");
+	}
+	
+	auto const roles = get_permitted_roles(internal::mod_action_name::BAN);
+	bool command_permitted{true};
+
+	if (roles.empty() && !author_top_role->has_ban_members() || (!roles.empty() && !author_top_role->has_ban_members() && !roles.contains(author_top_role))) {
+		cancel_operation = true;
+		command_permitted = false;
+		errors.emplace_back("❌ You do not have permission to run this command.");
+	}
+	if(command_permitted)
+		check_ban_possible(protected_roles);
 
 	if(cancel_operation) {
-		auto organized_errors = join_with_limit(errors, bot_max_embed_chars);
-		auto time_now = std::time(nullptr);
+		auto const organized_errors = join_with_limit(errors, bot_max_embed_chars);
+		auto const time_now = std::time(nullptr);
 		auto base_embed = dpp::embed()
 		                          .set_title("Error while banning member(s): ")
 		                          .set_color(ERROR_COLOR)
 		                          .set_timestamp(time_now);
-		if (organized_errors.size() == 1) {
+		if(organized_errors.size() == 1) {
 			base_embed.set_description(organized_errors[0]);
 			error_message.add_embed(base_embed);
 		}
 		else {
-			for (auto const &error: organized_errors) {
+			for(auto const &error: organized_errors) {
 				auto embed{base_embed};
 				embed.set_description(error);
 				error_message.add_embed(embed);
@@ -548,20 +525,7 @@ void ban_wrapper::check_permissions() {
 				paginator.start();
 			}
 		}
-		else {
-			auto webhook_url_query = transaction.exec_prepared("botlog", std::to_string(command.guild->id));
-			transaction.commit();
-			auto bot_error_webhook_url_field = webhook_url_query[0]["bot_error_logs"];
-			if(!bot_error_webhook_url_field.is_null()) {
-				auto bot_error_webhook_url = bot_error_webhook_url_field.as<std::string>();
-				dpp::webhook bot_error_webhook{bot_error_webhook_url};
-				command.bot->execute_webhook(bot_error_webhook, error_message);
-			}
-			else {
-				error_message.set_content("This server hasn't set a channel for bot errors. So the errors are being "
-				                          "sent to your DMs:");
-				command.bot->direct_message_create(command.author->user_id, error_message);
-			}
-		}
+		else
+			invoke_error_webhook();
 	}
 }
