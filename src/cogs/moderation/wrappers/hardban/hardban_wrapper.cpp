cc7273fdc94b3b9f7722679ed4a141571181f065
Arshia Aghaei <arshia.aghaei@gmail.com>
2025-01-20 23:56:36 +0330

    Fixed indentation issues, implemented checl_hardban_possible, and used the new get_protected_roles() method, replaced all error webhook snippets with invoke_webhook_error() calls, and all command log snippets with a log_command_invoke() call.

--- src/cogs/moderation/wrappers/hardban/hardban_wrapper.cpp
+++ src/cogs/moderation/wrappers/hardban/hardban_wrapper.cpp
@@ -13,43 +13,43 @@
 
 
 void hardban_wrapper::wrapper_function() {
-	for(auto const& member_or_user: snowflakes) {
-		if (auto const member_pointer = std::get_if<member_ptr>(&member_or_user)) {
+	for(auto const &member_or_user: snowflakes) {
+		if(auto const member_pointer = std::get_if<member_ptr>(&member_or_user)) {
 			members.insert(*member_pointer);
 			users.insert((*member_pointer)->get_user());
 		}
-		else if(auto const* user_pointer = std::get_if<user_ptr>(&member_or_user)) {
+		else if(auto const *user_pointer = std::get_if<user_ptr>(&member_or_user)) {
 			users.insert(*user_pointer);
 		}
-		else { // Will never happen but failsafe
+		else {// Will never happen but failsafe
 			invalid_user = true;
 			errors.emplace_back("Invalid user.");
 			break;
 		}
 	}
 
-	if (invalid_user) {
+	if(invalid_user) {
 		auto const split = join_with_limit(errors, bot_max_embed_chars);
 
 		error_message = dpp::message(command.channel_id, "");
 		auto const time_now = std::time(nullptr);
-		auto base_embed	= dpp::embed()
-							.set_title("❌ Error while hard banning member(s): ")
-							.set_color(ERROR_COLOR)
-							.set_timestamp(time_now);
+		auto base_embed = dpp::embed()
+								  .set_title("❌ Error while hard banning member(s): ")
+								  .set_color(ERROR_COLOR)
+								  .set_timestamp(time_now);
 		if(split.size() == 1) {
 			base_embed.set_description(split[0]);
 			error_message.add_embed(base_embed);
 		}
 		else {
-			for (auto const &error: errors) {
+			for(auto const &error: errors) {
 				auto embed{base_embed};
 				embed.set_description(error);
 				error_message.add_embed(embed);
 			}
 		}
-		if (command.interaction) {
-			error_message.set_flags(dpp::m_ephemeral); // Hidden error message
+		if(command.interaction) {
+			error_message.set_flags(dpp::m_ephemeral);// Hidden error message
 			if(split.size() == 1)
 				(*command.interaction)->edit_response(error_message);
 			else {
@@ -57,8 +57,8 @@ void hardban_wrapper::wrapper_function() {
 				paginator.start();
 			}
 		}
-		else { // It's an auto mod action
-			auto transaction		 = pqxx::work{*command.connection};
+		else {// It's an auto mod action
+			auto transaction = pqxx::work{*command.connection};
 			auto error_channel_query = transaction.exec_prepared("botlog", std::to_string(command.guild->id));
 			if(error_channel_query.empty()) {
 				error_message.set_content("This server hasn't set a channel for bot errors. So the errors are being "
@@ -80,6 +80,52 @@ void hardban_wrapper::wrapper_function() {
 	process_response();
 }
 
+void hardban_wrapper::check_hardban_possible(shared_vector<dpp::role> const& protected_roles) {
+
+	auto const bot_user = std::make_unique<dpp::user>(command.bot->me);
+	auto const bot_member = find_guild_member(command.guild->id, bot_user->id);
+	auto const bot_roles = get_roles_sorted(bot_member);
+	auto const& bot_top_role = bot_roles.front();
+	auto const author_roles = get_roles_sorted(*command.author);
+	auto const& author_top_role = author_roles.front();
+
+	for(auto const &member: members) {
+		auto const member_roles = get_roles_sorted(*member);
+		auto const &member_top_role = member_roles.front();
+
+		if(member->is_guild_owner()) {// If for some reason you felt like hardbanning yourself, being the server owner.
+			cancel_operation = true;
+			errors.emplace_back("❌ Why are you trying to hard ban yourself, server owner? lmfao");
+		}
+		if(command.bot->me.id == member->user_id) {// If you decided to ban the bot (ReactAIO)
+			errors.emplace_back("❌ Can't hard ban myself lmfao.");
+			cancel_operation = true;
+		}
+
+		if(member_top_role->position > bot_top_role->position) {
+			errors.push_back(std::format("❌ {} has a higher role than the bot. Unable to hard ban. "
+								"Please move the bot role above the members and below your staff roles.",
+								member->get_mention()));
+			cancel_operation = true;
+		}
+
+		if(protected_roles.empty())
+			continue;
+
+		shared_vector<dpp::role> member_protected_roles;
+		reactaio::set_intersection(protected_roles, member_roles, member_protected_roles);
+		if(!member_protected_roles.empty()) {// If member has any of the protected roles.
+			cancel_operation = true;
+			std::vector<std::string> role_mentions;
+			std::ranges::transform(member_protected_roles, std::back_inserter(role_mentions), [](role_ptr const &role) {
+				return role->get_mention();
+			});
+			std::string role_mentions_str = join(role_mentions, " , ");
+			errors.push_back(std::format("❌ Member has the protected roles: {}. Cannot hard ban.", role_mentions_str));
+		}
+	}
+}
+
 void hardban_wrapper::check_permissions() {
 
 	if(members.empty() && users.empty()) { // No point if no member/user are there.
@@ -98,73 +144,31 @@ void hardban_wrapper::check_permissions() {
 	auto const bot_member = find_guild_member(command.guild->id, bot_user->id);
 	auto const bot_roles = get_roles_sorted(bot_member);
 	auto const& bot_top_role = bot_roles.front();
+	auto const author_roles = get_roles_sorted(*command.author);
+	auto const& author_top_role = author_roles.front();
 
-	pqxx::work transaction{*command.connection};
-	auto protected_roles_query = transaction.exec_prepared("protected_roles", std::to_string(command.guild->id));
-	transaction.commit();
-
-
-	shared_vector<dpp::role> protected_roles;
-
-	if(!protected_roles_query.empty()) {
-		auto const protected_roles_field = protected_roles_query[0]["protected_roles"];
-		internal::unique_vector<dpp::snowflake> protected_role_snowflakes = parse_psql_array<dpp::snowflake>(protected_roles_field);
-		reactaio::transform(protected_role_snowflakes, protected_roles, [](const dpp::snowflake role_id) {
-			return std::make_shared<dpp::role>(*find_role(role_id));
-		});
-	}
+	auto const protected_roles = get_protected_roles();
 
 	if(!bot_top_role->has_ban_members()) {
 		cancel_operation = true;
 		errors.emplace_back("❌ Bot doesn't have the appropriate permissions. Please make sure the Ban Members permission is enabled.");
 	}
 
-	for(auto const& member: members) {
-		auto const member_roles = get_roles_sorted(*member);
-		auto const& member_top_role = member_roles.front();
+	check_hardban_possible(protected_roles);
 
-		if(member->is_guild_owner()) { // If for some reason you felt like hardbanning yourself, being the server owner.
-			cancel_operation = true;
-			errors.emplace_back("❌ Why are you trying to hard ban yourself, server owner? lmfao");
-		}
-		if(command.bot->me.id == member->user_id) { // If you decided to ban the bot (ReactAIO)
-			errors.emplace_back("❌ Can't hard ban myself lmfao.");
-			cancel_operation = true;
-		}
-
-		if(!protected_roles.empty()) {
-			shared_vector<dpp::role> member_protected_roles;
-			reactaio::set_intersection(protected_roles, member_roles, member_protected_roles);
-			if(!member_protected_roles.empty()) { // If member has any of the protected roles.
-				cancel_operation = true;
-				std::vector<std::string> role_mentions;
-				std::ranges::transform(member_protected_roles, std::back_inserter(role_mentions), [](role_ptr const& role){
-					return role->get_mention();
-				});
-				std::string role_mentions_str = join(role_mentions, " , ");
-				errors.push_back(std::format("❌ Member has the protected roles: {}. Cannot hard ban.", role_mentions_str));
-			}
-		}
-
-		if(member_top_role->position > bot_top_role->position) {
-			errors.push_back(std::format("❌ {} has a higher role than the bot. Unable to hard ban. Please "
-										 "move the bot role above the members and below your staff roles.", member->get_mention()));
-			cancel_operation = true;
-		}
-	}
 	if(cancel_operation) {
 		auto const organized_errors = join_with_limit(errors, bot_max_embed_chars);
 		auto const time_now = std::time(nullptr);
 		auto base_embed = dpp::embed()
-								  .set_title("Error while hard banning member(s): ")
-								  .set_color(ERROR_COLOR)
-								  .set_timestamp(time_now);
-		if (organized_errors.size() == 1) {
+								.set_title("Error while hard banning member(s): ")
+								.set_color(ERROR_COLOR)
+								.set_timestamp(time_now);
+		if(organized_errors.size() == 1) {
 			base_embed.set_description(organized_errors[0]);
 			error_message.add_embed(base_embed);
 		}
 		else {
-			for (auto const &error: organized_errors) {
+			for(auto const& error: organized_errors) {
 				auto embed{base_embed};
 				embed.set_description(error);
 				error_message.add_embed(embed);
@@ -179,26 +183,13 @@ void hardban_wrapper::check_permissions() {
 				paginator.start();
 			}
 		}
-		else {
-			auto webhook_url_query = transaction.exec_prepared("botlog", std::to_string(command.guild->id));
-			transaction.commit();
-			auto const bot_error_webhook_url_field = webhook_url_query[0]["bot_error_logs"];
-			if(!bot_error_webhook_url_field.is_null()) {
-				auto bot_error_webhook_url = bot_error_webhook_url_field.as<std::string>();
-				dpp::webhook bot_error_webhook{bot_error_webhook_url};
-				command.bot->execute_webhook(bot_error_webhook, error_message);
-			}
-			else {
-				error_message.set_content("This server hasn't set a channel for bot errors. So the errors are being "
-										  "sent to your DMs:");
-				command.bot->direct_message_create(command.author->user_id, error_message);
-			}
-		}
+		else
+			invoke_error_webhook();
 	}
 }
 
 void hardban_wrapper::lambda_callback(const dpp::confirmation_callback_t &completion, user_ptr const &user) {
-	if (completion.is_error()) {
+	if(completion.is_error()) {
 		auto error = completion.get_error();
 		errors.push_back(std::format("❌ Unable to hard ban user **{}**. Error code {}: {}.", user->format_username(), error.code, error.human_readable));
 		users_with_errors.insert(user);
@@ -227,8 +218,8 @@ void hardban_wrapper::process_hardbans() {
 
 	auto* author_user = command.author->get_user();
 
-	for (auto const& user : users) {
-		if (command.interaction) { // If this was done in automod, DMing lots of users WILL result in a rate limit
+	for(auto const& user : users) {
+		if(command.interaction) { // If this was done in automod, DMing lots of users WILL result in a rate limit
 
 			std::string dm_message;
 
@@ -246,7 +237,7 @@ void hardban_wrapper::process_response() {
 	auto message = dpp::message(command.channel_id, "");
 	auto const* author_user = command.author->get_user();
 
-	if (has_error()) {
+	if(has_error()) {
 		auto format_split = join_with_limit(errors, bot_max_embed_chars);
 		auto const time_now = std::time(nullptr);
 		auto base_embed	= dpp::embed()
@@ -258,7 +249,7 @@ void hardban_wrapper::process_response() {
 			message.add_embed(base_embed);
 		}
 		else {
-			for (auto const& error : format_split) {
+			for(auto const& error : format_split) {
 				auto embed{base_embed};
 				embed.set_description(error);
 				message.add_embed(embed);
@@ -274,35 +265,10 @@ void hardban_wrapper::process_response() {
 				paginator.start();
 			}
 		}
-		else {
-			// Get bot error webhook
-			auto automod_log = dpp::message();
-			auto transaction  = pqxx::work{*command.connection};
-			auto webhook_url_query = transaction.exec_prepared("botlog", std::to_string(command.guild->id));
-			transaction.commit();
-			if(webhook_url_query.empty()) { // Bot has no error webhook set.
-				automod_log.set_content("This server hasn't set a channel for bot errors. So the errors are being sent to your DMs:");
-				for (auto const& error : format_split) {
-					auto embed{base_embed};
-					embed.set_description(error);
-					automod_log.add_embed(embed);
-				}
-				command.bot->direct_message_create(command.author->user_id, automod_log);
-			}
-			else {
-				auto webhook_url = webhook_url_query[0]["bot_error_logs"].as<std::string>();
-				dpp::webhook automod_webhook{webhook_url};
-				for (auto const& error : format_split) {
-					auto embed{base_embed};
-					embed.set_description(error);
-					automod_log.add_embed(embed);
-				}
-				command.bot->execute_webhook(automod_webhook, automod_log);
-			}
-
-		}
+		else
+			invoke_error_webhook();
 	}
-	if (!are_all_errors()) {
+	if(!are_all_errors()) {
 		shared_vector<dpp::user> hard_banned_users;
 		std::vector<std::string> hard_banned_usernames;
 		std::vector<std::string> hard_banned_mentions;
@@ -324,7 +290,7 @@ void hardban_wrapper::process_response() {
 		std::string description;
 		std::string gif_url;
 
-		if (hard_banned_users.size() == 1) {
+		if(hard_banned_users.size() == 1) {
 			title		= "Hard banned";
 			description = std::format("{} has been hard banned", usernames);
 			gif_url		= "https://media3.giphy.com/media/LOoaJ2lbqmduxOaZpS/giphy.gif";
@@ -443,18 +409,14 @@ void hardban_wrapper::process_response() {
 			command.bot->execute_webhook(public_modlog_webhook, log);
 		}
 	}
-	if (command.interaction) {
+	if(command.interaction) {
 		if(message.embeds.size() == 1)
 			(*command.interaction)->edit_response(message);
 		else {
 			message_paginator paginator{message, command};
 			paginator.start();
 		}
-		// Log command call
-		pqxx::work transaction{*command.connection};
-		transaction.exec_prepared("command_insert", std::to_string(command.guild->id), std::to_string(command.author->user_id),
-								  internal::mod_action_name::HARD_BAN, dpp::utility::current_date_time());
-		transaction.commit();
+		log_command_invoke(internal::mod_action_name::HARD_BAN);
 	}
 	else
 		command.bot->message_create(message);
