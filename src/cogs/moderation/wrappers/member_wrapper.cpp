45389fd2e979be1eba2212d846644fa1ed171f9b
Arshia Aghaei <arshia.aghaei@gmail.com>
2025-01-25 23:15:02 +0330

    Implemented log_modcase for member_wrapper

---
+++ src/cogs/moderation/wrappers/member_wrapper.cpp
@@ -0,0 +1,21 @@
+//
+// Created by arshia on 12/20/24.
+//
+
+#include "member_wrapper.h"
+
+#include "../../core/algorithm.h"
+
+
+void member_wrapper::log_modcase(std::string_view const& command_name) const {
+	auto transaction = pqxx::work{*command.connection};
+	auto const max_query = transaction.exec_prepared1("casecount", std::to_string(command.guild->id));
+
+	auto const max_id = max_query["case_id"].is_null() ? 1 : max_query["case_id"].as<std::uint64_t>() + 1;
+	shared_vector<dpp::guild_member> members_with_no_errors;
+	reactaio::set_difference(members, members_with_errors, members_with_no_errors);
+	for(auto const& member: members_with_no_errors)
+		transaction.exec_prepared("modcase_insert", std::to_string(command.guild->id), max_id, command_name,
+							  std::to_string(command.author->user_id), std::to_string(member->user_id), command.reason);
+	transaction.commit();
+}
