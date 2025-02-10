15ad964f4038d52709755a656469f7702e91179f
Arshia Aghaei <arshia.aghaei@gmail.com>
2025-02-10 23:17:31 +0330

    Added a check to insert the duration in the modcase if it exists.

--- src/cogs/moderation/wrappers/member_wrapper.cpp
+++ src/cogs/moderation/wrappers/member_wrapper.cpp
@@ -14,6 +14,13 @@ void member_wrapper::log_modcase(std::string_view const& command_name) const {
 	auto const max_id = max_query["case_id"].is_null() ? 1 : max_query["case_id"].as<std::uint64_t>() + 1;
 	shared_vector<dpp::guild_member> members_with_no_errors;
 	reactaio::set_difference(members, members_with_errors, members_with_no_errors);
+	if(duration) {
+		for(auto const& member: members_with_no_errors)
+			transaction.exec_prepared("modcase_insert_duration", std::to_string(command.guild->id), max_id, command_name, duration->to_string(),
+								  std::to_string(command.author->user_id), std::to_string(member->user_id), command.reason);
+		transaction.commit();
+		return;
+	}
 	for(auto const& member: members_with_no_errors)
 		transaction.exec_prepared("modcase_insert", std::to_string(command.guild->id), max_id, command_name,
 							  std::to_string(command.author->user_id), std::to_string(member->user_id), command.reason);
